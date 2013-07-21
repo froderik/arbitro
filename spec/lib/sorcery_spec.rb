268fdac47ea0a65bacbe813e0098d2dc2cb9b2e9
froderik <fredrik@eldfluga.se>
2013-07-21 16:20:01 +0200

    braces and indentation

--- spec/lib/sorcery_spec.rb
+++ spec/lib/sorcery_spec.rb
@@ -18,23 +18,23 @@ describe Sorcery do
     let(:heat1) { get_me_a_heat 2, 4, 7 }
     let(:heat2) { get_me_a_heat 3, 5, 6 }
     let(:heat3) { get_me_a_heat 4, 9, 4 }
-    let(:heats) { [heat1, heat2, heat3]}
+    let(:heats) { [heat1, heat2, heat3] }
 
     context 'standard case - ' do
       subject { Sorcery.score heats }
 
-      its(:sorted_ids) {should == [1,2,0]}
-      its(:winner) {should == 1}
-      it { subject[0].should == 9 }
-      it { subject[1].should == 18 }
-      it { subject[2].should == 17 }
+      its(:sorted_ids) { should == [1,2,0] }
+      its(:winner)     { should == 1 }
+      its([0])         { should == 9 }
+      its([1])         { should == 18 }
+      its([2])         { should == 17 }
     end
 
     context 'lower wins - ' do
       subject { Sorcery.score heats, condition: :min}
 
-      its(:sorted_ids) {should == [0,2,1]}
-      its(:winner) {should == 0}
+      its(:sorted_ids) { should == [0,2,1] }
+      its(:winner)     { should == 0 }
     end
   end
 
