907d7daaa07898dc83ccbf750fd218468d7da3be
froderik <fredrik@eldfluga.se>
2013-07-21 07:43:26 +0200

    refactor

--- lib/sorcery.rb
+++ lib/sorcery.rb
@@ -1,11 +1,14 @@
 
 module Sorcery
   class Results
+    def initialize 
+      @heats = []
+      @results = Hash.new 0
+    end
+
     def add_heat heat
-      @heats ||= []
       @heats << heat.results
 
-      @results ||= Hash.new 0
       heat.results.each do |one_result|
         @results[one_result.id] += one_result.score
       end
@@ -15,8 +18,12 @@ module Sorcery
       @results[id]
     end
 
-    def results
-      @results.to_a.sort {|x,y| y[1] <=> x[1]} .map { |result| result[0] }
+    def to_a
+      @results.to_a.sort {|x,y| y[1] <=> x[1]} 
+    end
+
+    def sorted_ids
+      to_a.map { |result| result[0] }
     end
   end
 
--- spec/lib/sorcery_spec.rb
+++ spec/lib/sorcery_spec.rb
@@ -14,14 +14,14 @@ describe Sorcery do
     Heat.new results
   end
 
-  context ' - some up some scores - maximum wins - ' do
+  context ' - sum up some scores - maximum wins - ' do
     let(:heat1) { get_me_a_heat 2, 4, 7 }
     let(:heat2) { get_me_a_heat 3, 5, 6 }
     let(:heat3) { get_me_a_heat 4, 9, 4 }
 
-    subject { totals = Sorcery.score [heat1, heat2, heat3] }
+    subject { Sorcery.score [heat1, heat2, heat3] }
 
-    its(:results) {should == [1,2,0]}
+    its(:sorted_ids) {should == [1,2,0]}
     it { subject[0].should == 9 }
     it { subject[1].should == 18 }
     it { subject[2].should == 17 }
